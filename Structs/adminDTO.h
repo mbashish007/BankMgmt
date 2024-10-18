b07e4b733883ed32c345a437837cede3e55ed539
mbashish007 <mbashishoo7@gmail.com>
2024-10-18 11:20:26 +0530

    chaged max_name_len

--- Structs/adminDTO.h
+++ Structs/adminDTO.h
@@ -4,12 +4,12 @@
 #define MAX_USERNAME_LEN 50
 #define MAX_STATUS_LEN 9 // For storing status
 #define MAX_ROLE_LEN 9
-#define MAX_Name_LEN 128
+#define MAX_NAME_LEN 128
 
 
 typedef struct {
     char username[MAX_USERNAME_LEN];    
-    char name[MAX_Name_LEN] ;
+    char name[MAX_NAME_LEN] ;
     char status[MAX_STATUS_LEN];
 } AdminDTO;
 
